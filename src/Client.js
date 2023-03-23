33f53d26452de6ad3a683b6251c2a6a32a6bc544
raphael.lima <raphael.lima.ti@gmail.com>
2023-03-23 09:03:02 -0300

    fix: added 5s to be able to change cyphertext

--- src/Client.js
+++ src/Client.js
@@ -520,7 +520,13 @@ class Client extends EventEmitter {
                 if (msg.isNewMsg) {
                     if(msg.type === 'ciphertext') {
                         // defer message event until ciphertext is resolved (type changed)
-                        msg.once('change:type', (_msg) => window.onAddMessageEvent(window.WWebJS.getMessageModel(_msg)));
+                        const timeoutCipher = setTimeout(() => {
+                            window.onAddMessageEvent(window.WWebJS.getMessageModel(msg));
+                        }, 5000);
+                        msg.once('change:type', (_msg) => {
+                            window.onAddMessageEvent(window.WWebJS.getMessageModel(_msg));
+                            clearTimeout(timeoutCipher);
+                        });
                     } else {
                         window.onAddMessageEvent(window.WWebJS.getMessageModel(msg)); 
                     }
